b3c0731fbbee26e909095d08b326fcdd426fbc69
aric.zheng <1290657123@qq.com>
2026-04-17 22:19:12 +0800

    refactor(lib): reorganize and group exports in main.ts
    - Separated private and public exports with clear comments
    - Reordered exports to logical groups: private, then components, then types
    - Improved code readability and maintainability for library users

--- packages/lib/src/main.ts
+++ packages/lib/src/main.ts
@@ -1,7 +1,14 @@
-export { useListContext } from "./use-list-context";
-export type { ListOptions } from "./use-list-context";
-export { DynamicList } from "./dynamic-list";
-export type { ListApi, ChangeEvent, ListAction } from "./types";
-export type { DynamicListProps } from "./dynamic-list";
-export { getList, setList, addToList, removeAt, updateAt } from "./store";
-export { subscribe, emitChange } from "./event";
+
+// private
+export { getList, setList, addToList, removeAt, updateAt } from './store';
+export { subscribe, emitChange } from './event';
+
+// public components
+export { useListContext } from './use-list-context';
+export { DynamicList } from './dynamic-list';
+
+// public types
+export type { ListOptions } from './use-list-context';
+export type { ListApi, ChangeEvent, ListAction } from './types';
+export type { DynamicListProps } from './dynamic-list';
+
